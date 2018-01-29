1b3e54f0b120911dba4a69b32ce4fb710a968584
Maxwell Schorer <mcschorer@gmail.com>
2018-01-29 00:33:57 -0800

    Assignment code.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 /* Sets a random integer quantity in range [1, 20] for each flavor. */
+function getRandomInt(n=20){
+  return 1 + Math.floor(Math.random()*n);
+}
+
+
 function setQuantities() {
-  // TODO
+  const flavors = document.getElementsByClassName("flavor");
+  let flavor, meta, child, span;
+
+  for (let i = 0; i < flavors.length; i++){
+    flavor = flavors[i];
+    meta = flavor.getElementsByClassName("meta")[0];
+    child = meta.firstChild;
+    span = document.createElement("span");
+    span.innerHTML = getRandomInt();
+    span.classList.add("quantity");
+    meta.insertBefore(span, child);
+  }
 }
 
 /* Extracts and returns an array of flavor objects based on data in the DOM. Each
@@ -13,37 +29,93 @@ function setQuantities() {
  * quantity: how many cups of the flavor are available
  */
 function extractFlavors() {
-  // TODO
+  let flavors = [];
+  let flavor, flavorDiv, descriptionDiv, metaDiv, description, name, childNodes;
+  const flavorDivs = document.getElementsByClassName("flavor");
+
+  for (let i = 0; i < flavorDivs.length; i++){
+    flavorDiv = flavorDivs[i]
+    flavor = {}
+    flavor["element"] = flavorDiv
+    descriptionDiv = flavorDiv.getElementsByClassName("description")[0]
+    flavor["name"] = descriptionDiv.getElementsByTagName("h2")[0].innerHTML
+    flavor["description"] = descriptionDiv.getElementsByTagName("p")[0].innerHTML
+    metaDiv = flavorDiv.getElementsByClassName("meta")[0]
+    flavor["quantity"] = parseInt(metaDiv.getElementsByClassName("quantity")[0].innerHTML,10)
+    flavor["price"] = parseFloat(metaDiv.getElementsByClassName("price")[0].innerHTML.slice(1))
+    flavors.push(flavor)
+  }
+  return flavors
 }
 
 /* Calculates and returns the average price of the given set of flavors. The
  * average should be rounded to two decimal places. */
 function calculateAveragePrice(flavors) {
-  // TODO
+  let totalSum = 0.0;
+
+  flavors.forEach(function(flavor){
+    totalSum += flavor.price
+  })
+
+  return (totalSum / flavors.length).toFixed(2)
 }
 
 /* Finds flavors that have prices below the given threshold. Returns an array
  * of strings, each of the form "[flavor] costs $[price]". There should be
  * one string for each cheap flavor. */
 function findCheapFlavors(flavors, threshold) {
-  // TODO
+  return flavors.filter(flavor => flavor.price < threshold).map(f => f.name + " costs $" + f.price)
 }
 
 /* Populates the select dropdown with options. There should be one option tag
  * for each of the given flavors. */
 function populateOptions(flavors) {
-  // TODO
+  const select = document.getElementsByTagName("select")[0]
+  select.innerHTML = ""
+  for (let i = 0; i < flavors.length; i++){
+    option = document.createElement("option")
+    option.innerHTML = flavors[i].name
+    option.setAttribute("value", String(i))
+    select.appendChild(option)
+  }
 }
 
 /* Processes orders for the given set of flavors. When a valid order is made,
  * decrements the quantity of the associated flavor. */
 function processOrders(flavors) {
-  // TODO
+  let form = document.getElementsByTagName("form")[0]
+  form.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    let input = form.getElementsByTagName("input")[0]
+    let quantity = parseInt(input.value,10)
+    if (isNaN(quantity)){return}
+    let select = form.getElementsByTagName("select")[0]
+    let flavorIndex = select.value
+
+    let flavorDiv = flavors[flavorIndex].element
+    //console.log(flavorDiv)
+    let flavorQuantity = flavorDiv.getElementsByClassName("meta")[0].getElementsByClassName("quantity")[0]
+    let curQuantity = parseInt(flavorQuantity.innerHTML)
+    if (quantity > curQuantity) {return};
+    flavorQuantity.innerHTML = (curQuantity - quantity);
+    input.value = ""
+  })
+
 }
 
 /* Highlights flavors when clicked to make a simple favoriting system. */
 function highlightFlavors(flavors) {
-  // TODO
+  for (let i = 0; i < flavors.length; i++){
+    let elem = flavors[i].element
+    elem.addEventListener("click", event => {
+      let classes = elem.classList
+      if (classes[classes.length-1] === "highlighted"){
+        classes.remove("highlighted")
+      } else {
+        classes.add("highlighted")
+      }
+    })
+  }
 }
 
 
